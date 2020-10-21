c1b2bea4c4d0263b84ed4c17dd3e2f99f7f7a374
rowenarono95 <rowenarono@gmail.com>
2020-10-21 23:46:25 +0300

    add password functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -11,6 +13,8 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -19,12 +23,13 @@ def create_app(config_name):
     # Creating the app configurations
 
     app.config.from_object(config_options[config_name])
-    # config_options[config_name].init_app(app)
+    config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
 
     # Registering the blueprint
@@ -37,5 +42,8 @@ def create_app(config_name):
     # setting config
     from .request import configure_request
     configure_request(app)
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     return app
--- config.py
+++ config.py
@@ -5,7 +5,22 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:roro254@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'rowenarono@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
@@ -18,4 +33,5 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+
 }
